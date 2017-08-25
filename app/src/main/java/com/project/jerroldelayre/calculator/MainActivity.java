56b4bc7e7c84071e24bbe1b24cc70ac89ec6270c
delayrejerrol <delayrejerrol@yahoo.com>
2017-08-24 16:57:53 -0800

    First Commit

---
+++ app/src/main/java/com/project/jerroldelayre/calculator/MainActivity.java
@@ -0,0 +1,188 @@
+package com.project.jerroldelayre.calculator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    EditText editTextInfo;
+    EditText editTextOutput;
+
+    private DecimalFormat df = new DecimalFormat("#.##########");
+    private String sCurrentOperator = "", sPreviousOperator = "", sHistory = "", sCurrentNumber = "", sPreviousNumber = "", sTotal = "", sLastIndex = "";
+    private double dTotal = 0;
+
+    private int iHistoryCount = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextInfo = (EditText) findViewById(R.id.edittext_info);
+        editTextOutput = (EditText) findViewById(R.id.edittext_output);
+    }
+
+    public void getValue(View view) {
+        String getValue = ((Button) view).getText().toString();
+
+        if (getValue.contains("C") || getValue.contains("CE")) {
+            clear();
+            return;
+        }
+
+        boolean isNonNumber = getValue.equals("+") || getValue.equals("-") || getValue.equals("*") || getValue.equals("/") || getValue.equals("=");
+        if (isNonNumber) {
+            if (sCurrentNumber.equals("")) return;
+            if (!sLastIndex.equals("")) {
+                return;
+            }
+            if (!getValue.equals("=")) {
+                sCurrentOperator = getValue;
+                sPreviousNumber = sCurrentNumber;
+                sHistory += sPreviousNumber + sCurrentOperator;
+                if (sPreviousOperator.equals("")) sPreviousOperator = sCurrentOperator;
+            }
+            iHistoryCount++;
+            switch (getValue) {
+                case "+":
+                    computeNumber();
+                    break;
+                case "-":
+                    computeNumber();
+                    //editTextInfo.setText(sHistory);
+                    break;
+                case "*":
+                    computeNumber();
+                    //editTextInfo.setText(sHistory);
+                    break;
+                case "/":
+                    computeNumber();
+                    //editTextInfo.setText(sHistory);
+                    break;
+                case "=":
+                    if (sLastIndex.equals("")) {
+                        sPreviousNumber = editTextOutput.getText().toString();
+                        Log.i(TAG, "sLastIndex: " + sLastIndex + " sPreviousNumber: " + sPreviousNumber);
+                    }
+                    computeNumber();
+                    editTextInfo.setText("");
+                    break;
+            }
+            sPreviousOperator = sCurrentOperator;
+            sCurrentOperator = "";
+            sCurrentNumber = "";
+            if (!getValue.equals("=")) {
+                sLastIndex = getValue;
+            }
+        } else {
+            if (sCurrentNumber.contains(".") && getValue.equals(".")) return;
+            if (getValue.equals(".") && sCurrentNumber.equals("")) {
+                sCurrentNumber = "0";
+            }
+            sCurrentNumber += getValue;
+            editTextOutput.setText(sCurrentNumber);
+            sLastIndex = "";
+        }
+    }
+
+    private void clear() {
+        editTextInfo.setText("");
+        editTextOutput.setText("");
+        sCurrentOperator = "";
+        sPreviousOperator = "";
+        sHistory = "";
+        sCurrentNumber = "";
+        sPreviousNumber = "";
+        sTotal = "";
+        sLastIndex = "";
+        dTotal = 0;
+        iHistoryCount = 0;
+    }
+
+    private void clearEqual() {
+        editTextInfo.setText("");
+        //editTextOutput.setText("");
+        sCurrentOperator = "";
+        sPreviousOperator = "";
+        sHistory = "";
+        sCurrentNumber = "";
+        sPreviousNumber = "";
+        sTotal = "";
+        sLastIndex = "";
+        dTotal = 0;
+        iHistoryCount = 0;
+    }
+
+    private void addNumber() {
+        //Log.i(TAG, "" + sPreviousNumber + sPreviousOperator + dTotal);
+        double previousNumber = Double.parseDouble(sPreviousNumber);
+        if (iHistoryCount == 1) {
+            dTotal = previousNumber + 0;
+            editTextInfo.setText(sHistory);
+            editTextOutput.setText(df.format(previousNumber));
+            return;
+        }
+        dTotal += previousNumber;
+        editTextInfo.setText(sHistory);
+        editTextOutput.setText(df.format(dTotal));
+    }
+
+    private void subtractNumber() {
+        double previousNumber = Double.parseDouble(sPreviousNumber);
+        if (iHistoryCount == 1) {
+            dTotal = previousNumber - 0;
+            editTextInfo.setText(sHistory);
+            editTextOutput.setText(df.format(previousNumber));
+            return;
+        }
+        dTotal -= previousNumber;
+        editTextInfo.setText(sHistory);
+        editTextOutput.setText(df.format(dTotal));
+    }
+
+    private void multiplyNumber() {
+        double previousNumber = Double.parseDouble(sPreviousNumber);
+        if (iHistoryCount == 1) {
+            dTotal = previousNumber * 1;
+            editTextInfo.setText(sHistory);
+            editTextOutput.setText(df.format(previousNumber));
+            return;
+        }
+        dTotal *= previousNumber;
+        editTextInfo.setText(sHistory);
+        editTextOutput.setText(df.format(dTotal));
+    }
+
+    private void divideNumber() {
+        double previousNumber = Double.parseDouble(sPreviousNumber);
+        if (iHistoryCount == 1) {
+            dTotal = previousNumber;
+            editTextInfo.setText(sHistory);
+            editTextOutput.setText(df.format(previousNumber));
+            return;
+        }
+        dTotal /= previousNumber;
+        editTextInfo.setText(sHistory);
+        editTextOutput.setText(df.format(dTotal));
+    }
+
+    private void computeNumber() {
+        if (sPreviousOperator.equals("+")) {
+            addNumber();
+        } else if (sPreviousOperator.equals("-")) {
+            subtractNumber();
+        } else if (sPreviousOperator.equals("*")) {
+            multiplyNumber();
+        } else if (sPreviousOperator.equals("/")) {
+            divideNumber();
+        }
+    }
+}
